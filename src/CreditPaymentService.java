91f4f823499b0e5acb68b2822e055a2b9ad5e82c
Aleks Gusev <algusb@mail.ru>
2022-07-31 15:27:05 +0300

    modification

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,13 +1,12 @@
 public class CreditPaymentService {
-    public float calculate(float a, float b, float c) {
-        // a - сумма кредита, b - количество месяцев, c - годовая процентная ставка
-        float c1 = c / 1200;
-        float b1 = 1 + c1;
-        float b2 = (float) Math.pow(b1, b);
-        float b3 = b2 - 1;
-        float c2 = c1 / b3;
-        float c3 = c1 + c2;
-        float result = a * c3;
+    public float calculate(float amount, float month, float percent) {
+        float monthPercent = percent / 1200;
+        float monthPercentPlusOne = 1 + monthPercent;
+        float timePeriodCoeff = (float) Math.pow(monthPercentPlusOne, month);
+        float timePeriodCoeffMinusOne = timePeriodCoeff - 1;
+        float coefficient = monthPercent / timePeriodCoeffMinusOne;
+        float annuityCoefficient = monthPercent + coefficient;
+        float result = amount * annuityCoefficient;
         return result;
     }
 }
